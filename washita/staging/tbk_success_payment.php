a1803419f283778c63ab458e728c71c5d017f34d
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-10-02 20:29:01 -0300

    WEBPAY PLUS LISTO

--- washita/staging/tbk_success_payment.php
+++ washita/staging/tbk_success_payment.php
@@ -2,6 +2,7 @@
 include_once(dirname(__FILE__)."/templates/header.general.php");
 require_once(dirname(__FILE__)."/php/transbank/OrdersGenerator.php");
 $USER = WashitaUser::CurrentUser();
+print_R($USER);
 $order = new OrderGenerator($USER->Id);
 $data_order = $order->ORDER_INFO($_POST["TBK_ORDEN_COMPRA"]);
 ?>
