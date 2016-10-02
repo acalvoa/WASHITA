d20f10f991fe654fc19e9c52124b4dbba356d285
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-10-02 13:50:06 -0300

    WEBPAY LISTO

--- washita/staging/php/transbank/test/db_test.php
+++ washita/staging/php/transbank/test/db_test.php
@@ -13,6 +13,7 @@ $ta = array("palo","palo","palo","palo");
 // CAMA DE PRUEBAS
 $de = '{"NAME":"Angelo Calvo Alfaro","ADDRESS":"Colombia 2055, San Ramon","EMAIL":"angelo.calvoa@gmail.com","PHONE":"954081153","WEIGHT":"1.00","IS_IRONING":null,"DISCOUNT_COUPON":"","PRICE_WITH_DISCOUNT":"1400","PRICE_WITHOUT_DISCOUNT":"1400","CREATE_DATE":"2016-10-02 16:34:39","PICKUP_FROM":"2016-10-03 08:00:00","PICKUP_TILL":"2016-10-03 08:00:00","IS_ONLY_IRONING":null,"DROPOFF_FROM":"2016-10-04 08:00:00","DROPOFF_TILL":"2016-10-04 08:00:00","ACTUAL_WEIGHT":null,"ADDITIONAL_PRICE_WITHOUT_DISCOUNT":null,"ADDITIONAL_PRICE_WITH_DISCOUNT":null,"IS_FEEDBACK_REQUESTED":0,"FEEDBACK_CODE":null,"CITY_AREA_ID":4,"WASH_TYPE":0,"COMMENT":"TEST WEBAY","ACTUAL_PRICE_WITH_DISCOUNT":null}';
 $insert3 = json_decode($de);
+$insert3 = (array)$insert3;
 $QUERY = $mysql->INSERT($insert3, "orders"); 
 // $QUERY = $mysql->M_INSERT($in, $ta);
 // $QUERY = $mysql->UPDATE($insert,$insert2,"palo"); 
