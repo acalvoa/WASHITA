15f24180b0500166a8ea52e4bfbfddb69a6abcbb
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-10-09 17:19:52 -0300

    SE INTEGRA ONECLICK

--- washita/staging/php/transbank/OneClickWs.php
+++ washita/staging/php/transbank/OneClickWs.php
@@ -122,7 +122,7 @@ class OneClickWs
 
 	);
 
-	function __construct($url='https://webpay3g.orangepeople.cl/webpayserver/wswebpay/OneClickPaymentService?wsdl')
+	function __construct($url='https://webpay3gint.transbank.cl/webpayserver/wswebpay/OneClickPaymentService?wsdl')
 	{
 		$this->soapClient = new OneClickSoap($url,array("classmap"=>self::$classmap,"trace" => true,"exceptions" => true));
 	}
