62829a492d37c456170ac793657a29b89dba5888
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-10-10 17:38:14 -0300

    SE INTEGRA ONECLICK

--- washita/staging/php/transbank/MySQLDB.php
+++ washita/staging/php/transbank/MySQLDB.php
@@ -115,7 +115,9 @@ class MySQLDB implements DBCommon {
 		$this->LOGDB(json_encode($field));
 		$stmt = $this->MYSQL->prepare($query);
 		$this->LOGDB("Error: ".$this->MYSQL->error);
+		echo "<br>";
 		echo $query."<br>";
+		print_r($field)."<br>";
 		if(!$stmt){
 			throw new Exception("The statement is incorrect. Check the query", 7);
 		}
