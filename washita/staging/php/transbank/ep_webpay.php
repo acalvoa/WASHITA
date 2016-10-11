e2ac72180bfe721ca802fc050c1630550b09e191
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-10-11 20:23:52 -0300

    PSUH ACT

--- washita/staging/php/transbank/ep_webpay.php
+++ washita/staging/php/transbank/ep_webpay.php
@@ -6,20 +6,21 @@
 *	
 *	This sctipr is used like a endpoint of transbank process
 */
-require_once(dirname(__FILE__)."/Webpay.php");
-require_once(dirname(__FILE__)."/OneClick.php");
 
 $action = $_GET['action'];
 if(!empty($action) AND !is_null($action)){
 	if($action == "BEGIN_TRANS"){
+		require_once(dirname(__FILE__)."/Webpay.php");
 		$webpay = new Webpay();
 		$webpay->START_TRANS_WS();
 	}
 	else if($action == "VERIFY_TRANS"){
+		require_once(dirname(__FILE__)."/Webpay.php");
 		$webpay = new Webpay();
 		$webpay->VERIFY();
 	}
 	else if($action == "ONECLICK_INSCRIPTION"){
+		require_once(dirname(__FILE__)."/OneClick.php");
 		if(isset($_GET['ws']) && $_GET['ws'] == "true"){
 			$oneclick = new OneClick();
 			$oneclick->INIT_INSCRIPTION(true);
@@ -31,6 +32,7 @@ if(!empty($action) AND !is_null($action)){
 		}
 	}
 	else if($action == "FINISH_ONECLICK_INSCRIPTION"){
+		require_once(dirname(__FILE__)."/OneClick.php");
 		if(isset($_GET['ws']) && $_GET['ws'] == "true"){
 			$oneclick = new OneClick();
 			$oneclick->FINISH_INSCRIPTION($_POST['TBK_TOKEN'], true);
@@ -42,6 +44,7 @@ if(!empty($action) AND !is_null($action)){
 		}
 	}
 	else if($action == "AUTHORIZE_ONECLICK"){
+		require_once(dirname(__FILE__)."/OneClick.php");
 		if(isset($_GET['ws']) && $_GET['ws'] == "true"){
 			$oneclick = new OneClick();
 			$oneclick->AUTHORIZE($_POST['TBK_USER'],true);
