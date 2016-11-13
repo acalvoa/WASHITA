891704c7cb873fb9b12ab3610542e7a4eb92f4d7
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-11-13 01:18:34 -0300

    l

--- washita/staging/oneclick.php
+++ washita/staging/oneclick.php
@@ -66,52 +66,6 @@ if(is_null($user)){
 else
 {
 ?>
-<section>
-    <div class="container">
-      <div class="section-heading section-order">
-          <form id="checkout_form" method="post" action="<?php echo $GLOBALS['TBK_AUTHORIZE_ONECLICK'];?>">
-            <input type="hidden" name="ODC" value="<?php echo $order->OrderNumber; ?>" />
-            <input type="hidden" name="PRICE" value="<?php echo $order->ActualPriceWithDiscount; ?>" />
-            <div class="row item checkout-block oneclick-resumen">
-                <div class="input-group-vertical">
-                    <p>Monto a pagar: $<?php echo $order->ActualPriceWithDiscount ?></p>
-                </div>
-                <div class="input-group-horizontal">
-                    <div class="payelement">
-                        <div class="logofield" style="padding-top:5px;"><img class="webpay-logo" src="img/oneclick.png" height="80"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="row item checkout-block oneclick_tab">
-                <div class="input-group-vertical">
-                    <p>Elige La tarjeta de pago</p>
-                </div>
-                <div class="input-group-horizontal">
-                    <div class="tc_input_row">
-                        <select name="TBK_USER" class="form-control" required>
-                            <option value="-1">Seleccione la tarjeta de pago</option>
-                            <?php 
-                                $providers = OneClick::GETPROVIDERS();
-                                foreach ($providers as $provider) {
-                                    echo '<option value="'.$provider['TBK_USER'].'" >('.strtoupper($provider['CREDIT_CARD_TYPE']).')  XXXX XXXX XXXX '.$provider['LAST4NUMBER'].'</option>';
-                                }
-                            ?>                               
-                        </select>
-                    </div>
-                    <div class="tc_add_row">
-                        <button type="button" class="rm_tc_btn" id="add_tc_action">- Quitar tarjeta</button>
-                        <button type="button" class="add_tc_btn" id="add_tc_action">+ Agregar tarjeta</button>
-                    </div>
-                </div>
-            </div>
-            <div class="row item checkout_footer oneclick_tab">
-                <button type="submit" class="pay_btn hvr-glow">PAGAR</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-</section>
 
  <?php 
 }
