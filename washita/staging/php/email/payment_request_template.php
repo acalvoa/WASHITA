50509497370409c32cb40449c85e0475b1b524d4
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-11-12 23:13:48 -0300

    l

--- washita/staging/php/email/payment_request_template.php
+++ washita/staging/php/email/payment_request_template.php
@@ -64,7 +64,7 @@ function GetPaymentRequestBody(Order $order){
                     Para procesar este pedido, por favor completar el pago de '.MoneyFormat($order->ActualPriceWithDiscount).' haciendo click en el siguiente botón.
                 </p>
                 <a href="'.$GLOBALS['site_root'].'/pay.php?orderNumber='.$order->OrderNumber.'" >
-                        <div><img src="'.$GLOBALS['site_root'].'/img/webpayplus.jpg" height="50" style="float:left; margin-right: 20px;"/><div style="
+                        <div style="height:50px;"><img src="'.$GLOBALS['site_root'].'/img/webpayplus.jpg" height="50" style="float:left; margin-right: 20px;"/><div style="
                                                 padding: 10px 40px;
                                                 font-size: 18px;
                                                 color: #EBF3E9;
@@ -77,7 +77,7 @@ function GetPaymentRequestBody(Order $order){
                     ">Pagar con Webpay</div></div>
                     </a> <br>
                     <a href="'.$GLOBALS['site_root'].'/pay.php?orderNumber='.$order->OrderNumber.'" >
-                        <div><img src="'.$GLOBALS['site_root'].'/img/oneclick.png" height="50" style="float:left; margin-right: 20px;"/><div style="
+                        <div style="height:50px;"><img src="'.$GLOBALS['site_root'].'/img/oneclick.png" height="50" style="float:left; margin-right: 20px;"/><div style="
                                                 padding: 10px 40px;
                                                 font-size: 18px;
                                                 color: #EBF3E9;
