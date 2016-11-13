9ac4d053b8703c9ea3edbc24d8b4014014b7c96c
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-11-13 01:17:55 -0300

    l

--- washita/staging/oneclick.php
+++ washita/staging/oneclick.php
@@ -59,63 +59,7 @@ if($_POST){
 $user = WashitaUser::CurrentUser();
 if(is_null($user)){
 ?>
-<section>
-    <div class="container">
-         <div class="section-heading section-order">
-                <h1>Ingresar</h1>
-                <div class="divider"></div>
-                    <?php 
-                              if($error){
-                                  echo '
-                                  <div class="alert alert-danger">
-                                    <strong>¡Error!</strong> '.$error.'
-                                  </div>
-                                ';
-                              }
-                              
-                ?>
-                <form method="post" action="login.php"> 
-                    <div class="row item checkout-block border-between">
-                        <div class="col col-sm-6">
-                            <p>con tu email y contraseña</p>
-                            <div class="input-group">
-                                <span class="input-group-addon">@</span>
-                                <input type="email" name="email" class="form-control" placeholder="Email" maxlength="124" required/>
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon">&nbsp*&nbsp</span>
-                                <input type="password" name="password" class="form-control" placeholder="Contraseña" maxlength="124" required/>
-                            </div>
-                              
-                            <input type="submit" class="btn btn-info" value="Log-in" />
-                            
-                           
-                        </div>
-                        <div class="col col-sm-6">
-                            <p>o utilizando tu cuenta</p>
-                            <a href="login.php?provider=facebook" class="btn btn-block btn-social btn-facebook">
-                                <span class="fa fa-facebook"></span>
-                                 Facebook
-                            </a>
-                            <a href="login.php?provider=google" class="btn btn-block btn-social btn-google">
-                                <span class="fa fa-google"></span>
-                                Google
-                            </a>
-                        </div>	
-                    </div>
-                     <div class="row item">
-                             <p>
-                                ¿Primera vez? <b><a href="register.php">Regístrate aquí</a></b>
-                           </p>
-                           <p>
-                                ¿Olvidaste tu contraseña? <a href="password_restore.php">Recuperar contraseña</a>
-                           </p>
-                        </div>
-                    </div>
-                </form>
-        </div>
-    </div>
-</section>
+
 
 <?php
 }
